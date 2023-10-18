8b6e29e5b31753b56923b0e3f8c86cfa23cc13c4
Dilsher <dilsherb@gmail.com>
2023-10-18 16:07:31 +0300

    hamburger added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import 'src/style.css'
+// JavaScript code to toggle the mobile menu and icons
+const menuIcon = document.getElementById("menuIcon");
+const closeIcon = document.getElementById("closeIcon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+    closeIcon.classList.toggle("active");
+});
+
+closeIcon.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    menuIcon.classList.remove("active");
+    closeIcon.classList.remove("active");
+});
